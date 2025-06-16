ebb0007fe8f93304184bf8fe85ad0f599c4488e4
jaumrosa <joao.rosa5@outlook.com>
2025-06-16 20:16:43 -0300

    Removendo Switch com dicionário

--- Menus/Menu.cs
+++ Menus/Menu.cs
@@ -1,3 +1,5 @@
+using ScreenSound.Modelos;
+
 namespace ScreenSound.Menus;
 
 internal class Menu
@@ -5,9 +7,14 @@ internal class Menu
     public void ExibirTituloDaOpcao(string titulo)
     {
         int quantidadeDeLetras = titulo.Length;
-        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '=');
         Console.WriteLine(asteriscos);
         Console.WriteLine(titulo);
         Console.WriteLine(asteriscos + "\n");
+    }
+
+    public virtual void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
     }
 }
