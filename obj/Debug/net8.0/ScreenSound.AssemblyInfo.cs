004eeaed10f26a2d5e729adfb20c358f1002fb9e
jaumrosa <joao.rosa5@outlook.com>
2025-07-29 21:01:02 -0300

    Implementando avaliação e detalhes de album

--- obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
+++ obj/Debug/net8.0/ScreenSound.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb0007fe8f93304184bf8fe85ad0f599c4488e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b254015c0fc279770edc1f49dd3219c003351275")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
